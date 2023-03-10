c1499cf037690e0928d32e97e1382e31a5230881
youngjun <yj0913k@naver.com>
2023-03-10 15:57:59 +0900

    Initial commit

---
+++ src/test/java/hello/login/web/session/SessionManagerTest.java
@@ -0,0 +1,38 @@
+package hello.login.web.session;
+
+import hello.login.domain.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.http.HttpServletResponse;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SessionManagerTest {
+
+    SessionManager sessionManager = new SessionManager();
+
+    @Test
+    void sessionTest() {
+        //세션 생성
+        MockHttpServletResponse response = new MockHttpServletResponse(); //더미로 만들어줌
+        Member member = new Member();
+        sessionManager.createSession(member, response);
+
+        //요청에 응답 쿠키 저장
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setCookies(response.getCookies()); //mySessionId=1231232-1432-4-fjewf-we-fwe-f-..
+
+        //세션 조회
+        Object result = sessionManager.getSesstion(request);
+        assertThat(result).isEqualTo(member);
+
+        //세션 만료
+        sessionManager.expire(request);
+        Object expired = sessionManager.getSesstion(request);
+        assertThat(expired).isNull();
+
+    }
+}
